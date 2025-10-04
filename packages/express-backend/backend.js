c10bf3d2fba6763099479bf3ed189a42169c85b1
zen-watts <zen-watts-alpha@proton.me>
2025-10-04 14:59:09 -0700

    Getting users by ID

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -21,6 +21,8 @@ app.get("/users", (req, res) => {
     }
   });
 
+
+
 app.listen(port, () => {
   console.log(
     `Example app listening at http://localhost:${port}`
@@ -33,6 +35,18 @@ const findUserByName = (name) => {
   );
 };
 
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+  
+app.get("/users/:id", (req, res) => {
+    const id = req.params["id"]; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send("Resource not found.");
+    } else {
+        res.send(result);
+    }
+});
 
 const users = {
     users_list: [
