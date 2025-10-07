6f93ff57841bcccada46cfc2d2b7bfbf002355eb
zen-watts <zen-watts-alpha@proton.me>
2025-10-06 17:04:38 -0700

    Using the POST method

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -6,6 +6,61 @@ const port = 8000;
 
 app.use(express.json());
 
+app.listen(port, () => {
+    console.log(
+      `Example app listening at http://localhost:${port}`
+    );
+  });
+
+const users = {
+    users_list: [
+        {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+        },
+        {
+        id: "abc123",
+        name: "Mac",
+        job: "Influencer"
+        },
+        {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+        },
+        {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+        },
+        {
+        id: "zap555",
+        name: "Dennis",
+        job: "Spy"
+        }
+    ]
+};
+
+
+/* GET Helpers */
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+const findUserById = (id) =>
+    users["users_list"].find((user) => user["id"] === id);
+
+/* POST Helpers */
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
+/* GET Endpoints */
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
@@ -21,22 +76,6 @@ app.get("/users", (req, res) => {
     }
   });
 
-
-
-app.listen(port, () => {
-  console.log(
-    `Example app listening at http://localhost:${port}`
-  );
-});
-
-const findUserByName = (name) => {
-  return users["users_list"].filter(
-    (user) => user["name"] === name
-  );
-};
-
-const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
   
 app.get("/users/:id", (req, res) => {
     const id = req.params["id"]; //or req.params.id
@@ -48,32 +87,10 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
-const users = {
-    users_list: [
-      {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-      },
-      {
-        id: "abc123",
-        name: "Mac",
-        job: "Influencer"
-      },
-      {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-      },
-      {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-      },
-      {
-        id: "zap555",
-        name: "Dennis",
-        job: "Spy"
-      }
-    ]
-  };
+/* POST Endpoints */
+  
+app.post("/users", (req, res) => {
+    const userToAdd = req.body;
+    addUser(userToAdd);
+    res.send();
+});
