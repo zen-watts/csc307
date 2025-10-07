10ad2191a528beb400777049131c49c8d27dd445
zen-watts <zen-watts-alpha@proton.me>
2025-10-07 13:21:22 -0700

    Finished IA 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -44,20 +44,28 @@ const users = {
 
 
 /* GET Helpers */
-const findUserByName = (name) => {
-    return users["users_list"].filter(
-      (user) => user["name"] === name
-    );
-  };
-  
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
+
+const filterUsers = ({ name, job }) => {
+    return users.users_list.filter(u =>
+        (name === undefined || name === u.name) &&
+        (job === undefined || job === u.job));
+}
 
 /* POST Helpers */
 const addUser = (user) => {
     users["users_list"].push(user);
     return user;
 };
+
+/* DELETE Helpers */
+const deleteUserById = (id) => {
+    const before = users.users_list.length;
+    users.users_list = users.users_list.filter(u => String(u.id) !== String(id));
+    return users.users_list.length < before; // Seems reasonable
+}
+
 
 /* GET Endpoints */
 
@@ -66,14 +74,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/users", (req, res) => {
-    const name = req.query.name;
-    if (name != undefined) {
-      let result = findUserByName(name);
-      result = { users_list: result };
-      res.send(result);
-    } else {
-      res.send(users);
-    }
+    const { name, job } = req.query;
+    const result = filterUsers({ name, job });
+    res.json({ users_list: result });
   });
 
   
@@ -87,10 +90,23 @@ app.get("/users/:id", (req, res) => {
     }
 });
 
+
 /* POST Endpoints */
   
 app.post("/users", (req, res) => {
     const userToAdd = req.body;
     addUser(userToAdd);
     res.send();
+});
+
+/* DELETE Endpoints */
+
+// remove a particular user by id from the list
+app.delete("/users/:id", (req, res) => {
+    const id = req.params["id"];
+    const removed = deleteUserById(id);
+    if (!removed) {
+        res.status(404).send("Resource not found.");
+    }
+    res.status(204).end();
 });
