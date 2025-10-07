a00bd98717e9d4566d5e84aba8bbc1b3769ba4f3
zen-watts <zen-watts-alpha@proton.me>
2025-10-07 16:08:59 -0700

    added CORS and first HTTP request to frontend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,10 +1,13 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
 
+app.use(cors());
 app.use(express.json());
+
 
 app.listen(port, () => {
     console.log(
--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,5 @@
 // src/MyApp.jsx
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Table from "./Table";
 import Form from "./Form";
 
@@ -7,6 +7,18 @@ import Form from "./Form";
 
 function MyApp() {
     const [characters, setCharacters] = useState([]);
+
+    useEffect(() => {
+      fetchUsers()
+        .then((res) => res.json())
+        .then((json) => setCharacters(json["users_list"]))
+        .catch((error) => { console.log(error); });
+    }, [] );
+    
+    function fetchUsers() {
+      const promise = fetch("http://localhost:8000/users");
+      return promise;
+    }
 
     function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
