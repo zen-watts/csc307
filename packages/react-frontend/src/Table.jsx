96d1f44c0e235342c9e5910a90179bf28d3d9764
zen-watts <zen-watts-alpha@proton.me>
2025-10-07 18:30:33 -0700

    Show ID in table

--- packages/react-frontend/src/Table.jsx
+++ packages/react-frontend/src/Table.jsx
@@ -5,6 +5,7 @@ function TableHeader() {
     return (
       <thead>
         <tr>
+          <th>ID</th>
           <th>Name</th>
           <th>Job</th>
           <th>Remove Row</th>
@@ -16,7 +17,8 @@ function TableHeader() {
 function TableBody(props) {
     const rows = props.characterData.map((row, index) => {
       return (
-        <tr key={index}>
+        <tr key={index}> {/* TODO: consider changing to ID */}
+          <td>{row.id}</td>
           <td>{row.name}</td>
           <td>{row.job}</td>
           <td>
