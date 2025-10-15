2b6087f0a3868aaada70ec498dd2252ada244d66
zen-watts <zen-watts-alpha@proton.me>
2025-10-14 19:52:41 -0700

    database access: initial work

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -8,81 +8,11 @@ const port = 8000;
 app.use(cors());
 app.use(express.json());
 
-
 app.listen(port, () => {
     console.log(
       `Example app listening at http://localhost:${port}`
     );
   });
-
-const users = {
-    users_list: [
-        {
-        id: "xyz789",
-        name: "Charlie",
-        job: "Janitor"
-        },
-        {
-        id: "abc123",
-        name: "Mac",
-        job: "Influencer"
-        },
-        {
-        id: "ppp222",
-        name: "Mac",
-        job: "Professor"
-        },
-        {
-        id: "yat999",
-        name: "Dee",
-        job: "Aspring actress"
-        },
-        {
-        id: "zap555",
-        name: "Dennis",
-        job: "Spy"
-        }
-    ]
-};
-
-
-/* GET Helpers */
-const findUserById = (id) =>
-    users["users_list"].find((user) => user["id"] === id);
-
-const filterUsers = ({ name, job }) => {
-    return users.users_list.filter(u =>
-        (name === undefined || name === u.name) &&
-        (job === undefined || job === u.job));
-}
-
-/* POST Helpers */
-const addUser = (user) => {
-    user.id = randomId();
-    users["users_list"].push(user);
-    return user;
-};
-
-const randomLetters = (n) => {
-    let s = "";
-    for (let i = 0; i < n; i++) s += String.fromCharCode(97 + Math.floor(Math.random() * 26));
-    return s;
-  };
-  
-const randomDigits = (n) => {
-    return String(Math.floor(Math.random() * 10 ** n)).padStart(n, "0");
-}
-
-const randomId = () => {
-    return randomLetters(3) + randomDigits(3);
-}
-
-/* DELETE Helpers */
-const deleteUserById = (id) => {
-    const before = users.users_list.length;
-    users.users_list = users.users_list.filter(u => String(u.id) !== String(id));
-    return users.users_list.length < before; // Seems reasonable
-}
 
 
 /* GET Endpoints */
@@ -99,7 +29,7 @@ app.get("/users", (req, res) => {
 
   
 app.get("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
+    const id = req.params.id;
     let result = findUserById(id);
     if (result === undefined) {
         res.status(404).send("Resource not found.");
